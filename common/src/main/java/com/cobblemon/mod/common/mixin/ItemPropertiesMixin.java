973e730b4868aa9d0fad078f7b2510d9d97872a2
mia <15349355-miasmusa@users.noreply.gitlab.com>
2024-11-14 08:41:05 -0500

    license + remove unused imports

--- common/src/main/java/com/cobblemon/mod/common/mixin/ItemPropertiesMixin.java
+++ common/src/main/java/com/cobblemon/mod/common/mixin/ItemPropertiesMixin.java
@@ -1,12 +1,18 @@
+/*
+ * Copyright (C) 2023 Cobblemon Contributors
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/.
+ */
+
 package com.cobblemon.mod.common.mixin;
 
 import com.cobblemon.mod.common.entity.fishing.PokeRodFishingBobberEntity;
-import com.cobblemon.mod.common.item.interactive.PokerodItem;
 import net.minecraft.client.multiplayer.ClientLevel;
 import net.minecraft.client.renderer.item.ItemProperties;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.player.Player;
-import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
