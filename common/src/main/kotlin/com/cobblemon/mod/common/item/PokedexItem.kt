41bc14168a12cae20427c3f75f4097ee0864fd39
luke <hart.lucas02@gmail.com>
2024-12-15 00:24:38 -0700

    Changed distance to use player attribute

--- common/src/main/kotlin/com/cobblemon/mod/common/item/PokedexItem.kt
+++ common/src/main/kotlin/com/cobblemon/mod/common/item/PokedexItem.kt
@@ -13,6 +13,7 @@ import com.cobblemon.mod.common.client.pokedex.PokedexType
 import com.cobblemon.mod.common.entity.pokemon.PokemonEntity
 import com.cobblemon.mod.common.util.isLookingAt
 import com.cobblemon.mod.common.util.isServerSide
+import com.cobblemon.mod.common.util.traceFirstEntityCollision
 import net.minecraft.client.player.LocalPlayer
 import net.minecraft.server.level.ServerPlayer
 import net.minecraft.world.InteractionHand
@@ -37,6 +38,19 @@ class PokedexItem(val type: PokedexType): CobblemonItem(Item.Properties().stacks
         usedHand: InteractionHand
     ): InteractionResultHolder<ItemStack> {
         val itemStack = player.getItemInHand(usedHand)
+
+        if (player.isCrouching) {
+            val hitPokemon = player.traceFirstEntityCollision(
+                entityClass = PokemonEntity::class.java,
+                maxDistance = player.entityInteractionRange().toFloat()
+            )
+
+            // If the player is looking at a Pokémon within range, block Pokedex usage
+            if (hitPokemon != null && hitPokemon.isOwnedBy(player)) {
+                return InteractionResultHolder.pass(itemStack)
+            }
+        }
+
         if (world.isClientSide && player is LocalPlayer) {
             CobblemonClient.pokedexUsageContext.type = type
         }
